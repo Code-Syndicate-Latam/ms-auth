f81b74c0f4418f6752f08b1169d98b1ac86f1fbe
JVARGAS <jaider.vargas-n@mail.escuelaing.edu.co>
2025-12-24 11:47:09 -0500

    feat: add unit tests for AuthService and enhance TokenResponse

--- src/main/java/com/teamsoft/ms/auth/model/response/TokenResponse.java
+++ src/main/java/com/teamsoft/ms/auth/model/response/TokenResponse.java
@@ -2,8 +2,10 @@ package com.teamsoft.ms.auth.model.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class TokenResponse {
     @JsonProperty("access_token")
     String accessToken;
